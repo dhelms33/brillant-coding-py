d6cfa81cbfa7eb42171a15dbcb8d504521b32932
dhelms33 <derrickhelms10@gmail.com>
2025-10-12 23:20:53 -0600

    Added new method

--- sloth.py
+++ sloth.py
@@ -5,8 +5,9 @@ class Sloth:
         self.color = color
         self.species = species, 
         self.favorite_food = favorite_food
+        self.arr = arr
     
-    @value.setter
+  #  @value.setter
     def set_species(self):
         self.species = "Linneaus' two-toed sloth"
     
@@ -16,7 +17,13 @@ class Sloth:
     def start_dancing(self):
         return("Alright " + self.name + "is dancing!")
     
+    def bubble_sort_arr(self, arr):
+        i, j = 0
+        mid = len(arr)/2
+        for i in range(len(arr)):
+            if len()
+    
 if __name__ == "__main__":
     user_input = input("Please use this to define the attributes, mainly the name, of the slot")
     obj_instance = Sloth(user_input)
-    dance_sloth = obj_instance.start_dancing()``
+    dance_sloth = obj_instance.start_dancing()
