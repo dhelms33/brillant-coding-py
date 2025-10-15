f97d05391d6ef613eab67d9946a94a2fcd7cf34e
dhelms33 <derrickhelms10@gmail.com>
2025-10-15 12:50:16 -0600

    Added logic to bubble sort

--- sloth.py
+++ sloth.py
@@ -19,10 +19,16 @@ class Sloth:
     
     def bubble_sort_arr(self, arr):
         i, j = 0
+        swapped = False
         mid = len(arr)/2
         for i in range(len(arr-1)):
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
+                swapped = True
+        if not swapped:
+            break
+        return ("Is it true that array is sorted "
+                + swapped)
     
 if __name__ == "__main__":
     user_input = input("Please use this to define the attributes, mainly the name, of the slot")
