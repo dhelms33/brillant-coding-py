8aabcd609055d2a9be25ea12938bf2b993a07111
dhelms33 <derrickhelms10@gmail.com>
2025-11-07 19:48:04 -0700

    Added first order derivatives

--- diffeq.py
+++ diffeq.py
@@ -8,3 +8,10 @@ class DifferentialEquations:
             return(f"dy/g(y) = f(x)dx")
         except ValueError as e:
             return("This is not a diff eq!")
+        
+    def first_order_derivatives(self, function1, exponent):
+        try:
+            return(f"{function1} **({exponent-1})/({exponent-1}))"
+        except ValueError as e:
+            return("Please input a first order derivative")
+            
