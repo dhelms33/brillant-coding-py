f5895e8c864fa9ca875d1b61f66ed5f8272faff7
dhelms33 <derrickhelms10@gmail.com>
2025-11-15 03:30:28 -0700

    Added bleach.py

---
+++ bleach.py
@@ -0,0 +1,16 @@
+class Bleach:
+    def __init__(self, name, Ichigo, Rukia):
+        self.name = name
+        self.main_chars = []
+
+    def is_number_one(self, self.name):
+        if self.name.lower() == "ichigo":
+            return True
+        else:
+            return False
+    
+    def num_main_char(self.main_chars):
+        count_char = 0
+        for char in self.main_chars:
+            count_char+=1
+        return count_char
