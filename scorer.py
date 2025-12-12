c39519b164f73ff8a1c493b31007f29e1bfa4878
dhelms33 <derrickhelms10@gmail.com>
2025-12-11 21:49:44 -0700

    Added uppercase

--- scorer.py
+++ scorer.py
@@ -4,8 +4,12 @@ class Scorer:
     
     def generate_score(self.password):
         score = 0
+        uppercase = False
         if len(self.password) > 5:
             score = 5
         elif len(self.password) < 5:
             score = len(self.password)
+        if self.password.isupper():
+            uppercase = True
+            score *= 2
         return score
