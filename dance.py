7fc3c16a1bea95f3b5df44e1b229de6b76d72342
Dereck Helms <74729959+dhelms33@users.noreply.github.com>
2025-12-21 23:49:06 -0700

    Update dance.py

--- dance.py
+++ dance.py
@@ -3,6 +3,9 @@ class Dance:
     def __init__(self, dancing):
         self.dancing = dancing
     def move_genwrator(self, dancing):
+        dance_move = ""
         try:
+   
             if dancing:
                 dance_move = moves[rand.int]
+            return dance_move
