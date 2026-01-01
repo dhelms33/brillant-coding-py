215a24284978a701e822d070601053da42c7ca32
dhelms33 <derrickhelms10@gmail.com>
2026-01-01 13:34:05 -0700

    Added Reset

---
+++ reset.py
@@ -0,0 +1,16 @@
+import failed_login() from Scorer
+import random
+class Reset:
+    failed_attempts = 5
+    def __init__(self, attempt, code, failed_attempts):
+        self.attempt = attempt
+        self.code = code
+        
+    def send_reset_code():
+        numbers = [1,5,87,3,1,6,88,45,34]
+        reset_code = 0
+        if failed_login():
+            
+            for num in numbers:
+                reset_code += numbers[random.randint]
+            return("Sending reset_code", reset_code)
