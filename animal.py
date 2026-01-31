ef771a43e67f639ff1411c35e1d7d8973f86d65b
Dereck Helms <74729959+dhelms33@users.noreply.github.com>
2026-01-31 02:33:43 -0700

    Update animal.py

--- animal.py
+++ animal.py
@@ -1,2 +1,12 @@
 class Animal:
-    pass
+    def _init_(self, type, action, instinct):
+        self.type = type
+        self.action = action
+        self.instinct = instinct
+    def do_action(is_asleep, action, is_obedient):
+        if is_asleep:
+            return("Cannot do action, sleeping, please don't wake.")
+        elif is_obedient:
+            return("doing ", action)
+        else: 
+            return("not doing ", action, "not obedient")
