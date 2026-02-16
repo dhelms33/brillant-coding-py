0fe0099160c93e9edc68f33c12991ed505125d65
dhelms33 <derrickhelms10@gmail.com>
2026-02-15 17:48:15 -0700

    Added count_globes

--- CrashCourse.py
+++ CrashCourse.py
@@ -9,3 +9,16 @@ class CrashCourse:
                 return("Notes for", self.lesson, "gathered! ", self.notes)
         except ValueError:
             return("No lesson for ", self.lesson)
+    
+    def count_globes():
+        globes = 0
+        room = {
+            "back_left":1,
+            "back_right":0,
+            "front_right":1,
+            "front_left":2
+            }
+        for globe in room.values():
+            if value >= 1:
+                globe += 1
+        return globes
