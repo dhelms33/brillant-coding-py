5fa879df2326f2bf118eae2afe7f9ed6b55f4f64
dhelms33 <derrickhelms10@gmail.com>
2026-02-18 00:43:38 -0700

    Added count_demand

--- CrashCourse.py
+++ CrashCourse.py
@@ -28,3 +28,12 @@ class CrashCourse:
         for supply in range(10):
             supply_count += 1
         return supply_count
+    def count_demand(demand_int):
+        deamand_count = 0
+        for demand in range(demand_int):
+            demand_count += 1
+        return deamand_count
+    def determine_surplus():
+        if count_demand > count_supply:
+            
+        
