050b4aa09b25fe1cb18071f82854d05a554f06c3
Dereck Helms <74729959+dhelms33@users.noreply.github.com>
2026-03-02 16:32:04 -0700

    Create type_casting.py

---
+++ type_casting.py
@@ -0,0 +1,8 @@
+class Caster:
+
+    def __init__(self, score):
+        self.score = score
+    def  calc_score(): 
+        new_score = input("Enter a new score")
+        avg_score = (94 + 91 + 83.5 + int(new_score)) / 4
+        return("My average score is " +str(avg_score))
