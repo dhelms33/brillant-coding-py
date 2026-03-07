e235d7cb5bdb59611554d808e2a188af5184eaa5
dhelms33 <dereckhelmsdev@gmail.com>
2026-03-07 01:29:27 -0700

    Added calc_abs_squared

--- type_casting.py
+++ type_casting.py
@@ -6,3 +6,13 @@ class Caster:
         new_score = input("Enter a new score")
         avg_score = (94 + 91 + 83.5 + int(new_score)) / 4
         return("My average score is " +str(avg_score))
+    
+    def calc_shortest_name(name1, name2):
+        min_name = min(len(name1), len(name2))
+        return min_name
+    def calc_abs_squared(num):
+        try:
+            squared = abs(num)**2
+            return squared
+        except ValueError as {e}:
+            return("{e} is an invalid number, please try again!")
