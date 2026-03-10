00e8575dced1cf5f2baebbbc041ce9ddb8620c36
dhelms33 <derrickhelms10@gmail.com>
2026-03-09 23:06:44 -0600

    Added turn into dragon

--- reveal.py
+++ reveal.py
@@ -10,3 +10,13 @@ class Reveal:
             past_halfway = countdown >= duration/2
         return("countdown " + countdown + "past halfway? "+ past_halfway)
     
+    def turn_into_dragon(number_of_wings, legs):
+        try:
+            if min(number_of_wings, legs) != 2:
+                number_of_wings = 2
+                legs = 4
+                return("Dragons here have ", number_of_wings, " and ", legs ,"here")
+        except ValueError as {e}:
+            return("We did not understand that, please try again.")
+        
+    
